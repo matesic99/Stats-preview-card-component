dfa43916d51e3966de97799a1d788e99d50125bc
matesic99 <markomtsc@gmail.com>
2022-03-25 13:53:32 +0100

    stats-card

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    colors: {
+      'vdblue': 'hsl(233, 47%, 7%)',
+      'dsblue': 'hsl(244, 38%, 16%)',
+      'softviolet': 'hsl(277, 64%, 61%)',
+      'white': 'hsl(0, 0%, 100%)',
+      'stwhite': 'hsla(0, 0%, 100%, 0.75)',
+      'sthwite': 'hsla(0, 0%, 100%, 0.6)',
+      'gray-light': '#d3dce6',
+    },
+    
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        lexenda: ['Lexenda Deca', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
